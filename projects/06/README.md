252a11629956351b707d45bb2d2cbfa3aa75f231
Jeffrey Florek <hello@jeffreyflorek.com>
2023-05-15 08:58:24 -0400

    Add tests for code module

--- projects/06/README.md
+++ projects/06/README.md
@@ -3,5 +3,7 @@ My implementation of the assembler from Chapter 6 of nand2tetris [[1]](#1).
 
 [![Parser](https://github.com/jeffreyflorek/nand2tetris/actions/workflows/test-project-06-parser.yml/badge.svg)](https://github.com/jeffreyflorek/nand2tetris/actions/workflows/test-project-06-parser.yml)
 
+[![Code](https://github.com/jeffreyflorek/nand2tetris/actions/workflows/test-project-06-code.yml/badge.svg)](https://github.com/jeffreyflorek/nand2tetris/actions/workflows/test-project-06-code.yml)
+
 ## References
 <a id="1">[1]</a> Noam Nisan and Shimon Schocken. 2021. The Elements of Computing Systems, Second Edition: Building a Modern Computer from First Principles, MIT Press.
---
+++ projects/06/tests/test_code.py
@@ -0,0 +1,73 @@
+from assembler import code
+import pytest
+
+
+@pytest.mark.parametrize(
+    "dest, expected_code",
+    [
+        (None, "000"),
+        ("M", "001"),
+        ("D", "010"),
+        ("DM", "011"),
+        ("A", "100"),
+        ("AM", "101"),
+        ("AD", "110"),
+        ("ADM", "111"),
+    ],
+)
+def test_dest(dest, expected_code):
+    assert code.dest(dest) == expected_code
+
+
+@pytest.mark.parametrize(
+    "comp, expected_code",
+    [
+        ("0", "0101010"),
+        ("1", "0111111"),
+        ("-1", "0111010"),
+        ("D", "0001100"),
+        ("A", "0110000"),
+        ("M", "1110000"),
+        ("!D", "0001101"),
+        ("!A", "0110001"),
+        ("!M", "1110001"),
+        ("-D", "0001111"),
+        ("-A", "0110011"),
+        ("-M", "1110011"),
+        ("D+1", "0011111"),
+        ("A+1", "0110111"),
+        ("M+1", "1110111"),
+        ("D-1", "0001110"),
+        ("A-1", "0110010"),
+        ("M-1", "1110010"),
+        ("D+A", "0000010"),
+        ("D+M", "1000010"),
+        ("D-A", "0010011"),
+        ("D-M", "1010011"),
+        ("A-D", "0000111"),
+        ("M-D", "1000111"),
+        ("D&A", "0000000"),
+        ("D&M", "1000000"),
+        ("D|A", "0010101"),
+        ("D|M", "1010101"),
+    ],
+)
+def test_comp(comp, expected_code):
+    assert code.comp(comp) == expected_code
+
+
+@pytest.mark.parametrize(
+    "jump, expected_code",
+    [
+        (None, "000"),
+        ("JGT", "001"),
+        ("JEQ", "010"),
+        ("JGE", "011"),
+        ("JLT", "100"),
+        ("JNE", "101"),
+        ("JLE", "110"),
+        ("JMP", "111"),
+    ],
+)
+def test_jump(jump, expected_code):
+    assert code.jump(jump) == expected_code
